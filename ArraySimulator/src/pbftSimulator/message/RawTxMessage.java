bb3d0b284a7d658641df237df1f9195af40c8ca4
OnlyCaptain <1594962496@qq.com>
2021-06-14 19:42:20 +0800

    添加了RawTxMessage的encoder跟decoder

--- ArraySimulator/src/pbftSimulator/message/RawTxMessage.java
+++ ArraySimulator/src/pbftSimulator/message/RawTxMessage.java
@@ -1,24 +1,41 @@
 package pbftSimulator.message;
 
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
 import shardSystem.transaction.Transaction;
 
 public class RawTxMessage extends Message {
 	private Transaction tx;
-	public RawTxMessage(Transaction tx) {
+	private JSONArray m;   // transactions: [JSONObject, JSONObject, ... ]
+	
+	public RawTxMessage(Transaction[] txs) {
 		super(0, 0, 0);
-		this.tx = tx;
-		this.type = REQUEST;
-		this.len = REQMSGLEN;
-		// TODO Auto-generated constructor stub
+		// this.tx = tx;
+		m = new JSONArray();
+		for (int i = 0; i < txs.length; i ++) {
+			m.add(txs[i]);
+		}
+		this.type = Message.TRANSACTION;
 	}
 
 	public RawTxMessage(String jsbuff) {
-		
+		try {
+			JSONObject js = JSONObject.fromObject(jsbuff);
+			rcvId = js.getInt("rcvId");
+			rcvtime = js.getLong("rcvtime");
+			sndId = js.getInt("sndId");
+			m = js.getJSONArray("m");
+			len = js.getLong("len");
+			type = js.getInt("type");
+		} catch (Exception e) {
+			System.out.println("json 转换失败"+e.getMessage());
+		} 
 	}
 
-	public RawTxMessage() {
-		// TODO Auto-generated constructor stub
-	}
+	// public RawTxMessage() {
+	// 	this.m = null;
+	// 	// TODO Auto-generated constructor stub
+	// }
 
 	public Transaction getTx() {
 		return tx;
@@ -26,6 +43,31 @@ public class RawTxMessage extends Message {
 
 	public void setTx(Transaction tx) {
 		this.tx = tx;
+	}
+
+	public String toString() {
+		return encoder();
+	}
+
+	public String encoder() {
+		JSONObject jsout = new JSONObject();
+		jsout.put("rcvId", rcvId);
+		jsout.put("rcvtime", rcvtime);
+		jsout.put("sndId", sndId);
+		jsout.put("len", len);
+		jsout.put("type", type);
+		jsout.put("m", m);
+		return jsout.toString();
+	}
+
+	public static void main(String[] args) {
+		Transaction tx1 = new Transaction("send", "recip", 1.11, null, 0, 100.0, 0);
+		// Transaction tx1 = new Transaction("send", "recip", 1.11, null, 0, 100, 0);
+		RawTxMessage rt = new RawTxMessage(new Transaction[] {tx1});
+		System.out.println(rt.toString());
+
+		RawTxMessage rt2 = new RawTxMessage(rt.toString());
+		System.out.println(rt2.toString());
 	}
 
 }

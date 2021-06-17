f17ce2d597747c22759afd698d961faf82d6504c
OnlyCaptain <1594962496@qq.com>
2021-06-17 22:02:27 +0800

    更新install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
-rm -rf ~/shardRelease
-wget --no-check-certificate --content-disposition https://gitee.com/OnlyCaptain/shardRelease/attach_files/741798/download/shardRelease.zip
-unzip shardRelease.zip
+rm -rf ~/shardRelease ~/shardRelease.zip
+wget --no-check-certificate --content-disposition https://gitee.com/OnlyCaptain/shardRelease/attach_files/742519/download/shardRelease.zip
 
 systemctl stop firewalld.service
 source /etc/profile;
@@ -8,5 +7,5 @@ cd shardRelease
 rm -rf workspace
 kill `netstat -anp | grep 60635 | awk '{split($7,b,"/"); print b[1]}' | sed -n '1p'`
 kill `netstat -anp | grep 58052 | awk '{split($7,b,"/"); print b[1]}' | sed -n '1p'`
-java -jar SingleShard.jar ./config.json
+java -jar MultiShard.jar ./config.json
 

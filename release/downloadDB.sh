c15c487c5a43eef0a61f883a3bf3651650194d7a
OnlyCaptain <1594962496@qq.com>
2021-07-29 20:45:31 +0800

    更新下载数据库脚本

--- release/downloadDB.sh
+++ release/downloadDB.sh
@@ -3,7 +3,7 @@ dbfile=$1
 user="root"
 pwd="NOR4myself!"
 ips=(
-"120.78.190.223" "119.23.64.121" "120.77.170.151"
+"47.106.169.130" "47.106.203.100" "47.106.191.65" "120.79.129.137" "47.106.167.80" "39.108.111.231" "47.106.73.62" "47.106.104.251" "47.106.183.146" "47.106.117.225" "47.106.10.253" "47.106.12.4" "47.106.113.7" "120.78.190.223" "119.23.64.121" "120.77.170.151"
 )
 port=22
 target="/root/shardSimulator/workspace/shard_*/*-sqlite.db"
@@ -11,7 +11,7 @@ recvf=./database/
 for ip in ${ips[*]}
 do
 	/usr/bin/expect<<-EOF   #shell中使用expect
-	set timeout 90
+	set timeout 3000
 	spawn scp -r -P $port $user@$ip:$target $recvf
 	expect {
         "yes/no" {send "yes\r"}
